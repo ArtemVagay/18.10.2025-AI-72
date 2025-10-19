b98b989d434c5b91b2df0903e73583c5d0024e85
ArtemVagay <artemvagay2012@gmail.com>
2025-10-19 16:54:53 +0700

    Added new code file

---
+++ Code_Unik.py
@@ -0,0 +1,73 @@
+import random
+import math
+island_size = 0
+while True:
+    island_size = int(input("Введите размер острова(минимум: 2): "))
+    if island_size > 1:
+        break
+    print("Ошибка!")
+matrix_size = island_size * 15
+island_count = random.randint(island_size - 1, island_size)
+print(island_count)
+matrix = [["🌊" for i in range(matrix_size)] for j in range(matrix_size)]
+
+def island_maker(radius, num_points):
+    random_angle = random.randint(-360, 360)
+    for i in range(num_points):
+        radius_minus = (random.randint(8, 11) / 10)
+        radius = radius * radius_minus
+        angle = angle = 2 * math.pi * i / num_points + random_angle
+        center_x = round(radius * math.cos(angle))
+        center_y = round(radius * math.sin(angle))
+        for y in range(matrix_size):
+            for x in range(matrix_size):
+                if x == center_x + matrix_size // 2 and y == center_y + matrix_size // 2:
+                    matrix[y][x] = "🌴"
+                for _ in range(matrix_size):
+                    if x == center_x + matrix_size // 2 + random.randint(-island_size // 2, island_size // 2) and y == center_y + matrix_size // 2 + random.randint(-island_size // 2, island_size // 2):
+                        matrix[y + random.randint(-1, 1)][x] = "🌴"
+                        matrix[y][x + random.randint(-1, 1)] = "🌴"
+                        matrix[y - random.randint(-1, 1)][x] = "🌴"
+                        matrix[y][x - random.randint(-1, 1)] = "🌴"
+                        matrix[y + random.randint(-1, 1)][x + random.randint(-1, 1)] = "🌴"
+
+
+island_maker(0, 1)
+island_maker(island_size * 5, island_count)
+
+leave = False
+
+while not leave:
+    if not leave:
+        for y in range(matrix_size):
+            if not leave:
+                for x in range(matrix_size):
+                    if matrix[y][x] == "🌴":
+                        rnd = random.randint(0, 1000)
+                        if x == matrix_size // 2 + random.randint(-island_size // 4, island_size // 4) and y == matrix_size // 2 + random.randint(-island_size // 4, island_size // 4):
+                            if rnd == 1:
+                                if matrix[y - 1][x] == "🌊":
+                                    matrix[y + 1][x] = "❌"
+                                elif matrix[y + 1][x] == "🌊":
+                                    matrix[y - 1][x] = "❌"
+                                elif matrix[y][x + 1] == "🌊":
+                                    matrix[y][x - 1] = "❌"
+                                elif matrix[y][x - 1] == "🌊":
+                                    matrix[y][x + 1] = "❌"
+                                elif matrix[y - 1][x - 1] == "🌊":
+                                    matrix[y + 1][x + 1] = "❌"
+                                elif matrix[y + 1][x + 1] == "🌊":
+                                    matrix[y - 1][x - 1] = "❌"
+                                elif matrix[y - 1][x + 1] == "🌊":
+                                    matrix[y + 1][x - 1] = "❌"
+                                elif matrix[y + 1][x - 1] == "🌊":
+                                    matrix[y - 1][x + 1] = "❌"
+                                else:
+                                    matrix[y][x] = "❌"
+                                leave = True
+                                break
+
+for row in matrix:
+    for element in row:
+        print(element, end=' ')
+    print()
